387547d6a39d1bad1309b8a9bd38fb1910d3eb89
Marcela-Gomez <marcelasaraigomezcanas@gmail.com>
2025-01-07 17:37:06 -0600

    feat: carga de destinos desde la base de datos

--- src/app/destinos/page.tsx
+++ src/app/destinos/page.tsx
@@ -1,46 +1,72 @@
-import Card from "@/app/components/Card";
-import Navbar from "../components/Navbar";
-import Styles from "./destinos.module.css";
+"use client"
+
+import { useEffect, useState } from "react";
 import Navbar_users from "../components/Navbar_users";
+import Styles  from "./destinos.module.css";
+import Card from "../components/Card";
 
-const destinos = [
-  {
-    title: "Casa moderna en Francia París",
-    description: "Un moderno apartamento en el corazón...",
-    image: "https://res.cloudinary.com/dnc8k8wwv/image/upload/v1734930771/residence-2219972_1280_r4jgcv.jpg",
-   },
-  {
-    title: "Casa moderna en Francia París",
-    description: "Un moderno apartamento en el corazón...",
-    image: "https://res.cloudinary.com/dnc8k8wwv/image/upload/v1736013464/door-8881474_1280_nzkidt.jpg",
-  },
-  {
-    title: "Casa moderna en Francia París",
-    description: "Un moderno apartamento en el corazón de paris con vistas a la ciudad.",
-    image: "https://res.cloudinary.com/dnc8k8wwv/image/upload/v1722751869/nycncsxudw5hzecqxrut.jpg",
-  },
-  {
-    title: "Casa moderna en Francia París",
-    description: "Un moderno apartamento en el corazón...",
-    image: "https://res.cloudinary.com/dnc8k8wwv/image/upload/v1722798177/c61yuzna57cwr0c2jwqt.jpg",
-  },
-];
+interface Destination {
+  id: number;
+  address: string;
+  country: string;
+  city: string;
+  price: number;
+  rooms: number;
+  bathrooms: number;
+  size: string;
+  image: string;
+  description: string;
+}
 
-export default function Destinos() {
+export default function Destinations() {
+  const [destinations, setDestinations] = useState<Destination[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchDestinations() {
+      try {
+        const res = await fetch("http://localhost:3000/api/destinations");
+        const result = await res.json();
+        if (!res.ok) throw new Error(result.error);
+        const formattedDestinations = result.destinations.map((destino: any) => ({
+          id: destino.ACCOMODATION_ID,
+          address: destino.ADDRESS,
+          country: destino.COUNTRY,
+          city: destino.CITY,
+          price: destino.PRICE,
+          rooms: destino.ROOMS,
+          bathrooms: destino.BATHROOMS,
+          size: destino.SIZE,
+          image: destino.IMAGE,
+          description: destino.DESCRIPTION,
+        }));
+
+        setDestinations(formattedDestinations);
+      } catch (err: any) {
+        setError(err.message);
+      }
+    }
+
+    fetchDestinations();
+  }, []);
+
+  if (error) return <p>Error: {error}</p>;
+  if (!destinations.length) return <p>Cargando destinos...</p>;
+
   return (
-    <main>
-      <Navbar_users/>
-      <h1 className={Styles.tittle_destinos}>Mis destinos</h1>
+    <div>
+      <Navbar_users />
+      <h1 className={Styles.tittle_destinos}>Destinos</h1>
       <div className={Styles.container_card}>
-        {destinos.map((destino, index) => (
+        {destinations.map((destino) => (
           <Card
-            key={index}
-            title={destino.title}
-            description={destino.description}
-            image={destino.image}
+          key={destino.id}
+          title={destino.city}
+          description={destino.description}
+          image={destino.image}
           />
         ))}
       </div>
-    </main>
+    </div>
   );
 }
